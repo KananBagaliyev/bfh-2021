73100a62473e55d323471974c91a8ecf78841648
Kanan Bagaliyev <kanan@Kanans-MacBook-Pro.local>
2021-11-28 04:29:48 +0400

    minor_change

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/brandyol-angular'),
+      dir: require('path').join(__dirname, './coverage/revofood-angular'),
       subdir: '.',
       reporters: [
         { type: 'html' },
